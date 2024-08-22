50e31879622d85854a5da00fad24527be7bd11f8
Daniel Ramírez-Chiquillo <hi@danielrachi.com>
2024-08-22 16:05:11 -0500

    Initial commit

---
+++ test/pong_web/controllers/error_html_test.exs
@@ -0,0 +1,14 @@
+defmodule PongWeb.ErrorHTMLTest do
+  use PongWeb.ConnCase, async: true
+
+  # Bring render_to_string/4 for testing custom views
+  import Phoenix.Template
+
+  test "renders 404.html" do
+    assert render_to_string(PongWeb.ErrorHTML, "404", "html", []) == "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(PongWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+  end
+end
